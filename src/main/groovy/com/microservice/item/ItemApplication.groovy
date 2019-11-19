5838ab091fa1c2857f494a641c562ca7aa517479
Edson Srvn <edson@makingdevs.com>
2019-11-19 09:12:50 -0600

    coding master Initial commit

---
+++ src/main/groovy/com/microservice/item/ItemApplication.groovy
@@ -0,0 +1,13 @@
+package com.microservice.item
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ItemApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ItemApplication, args)
+	}
+
+}
